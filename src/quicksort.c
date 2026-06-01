cee4e3f078578c9e38c6872b28dd39bbdf7ecab2
LuisEn2005 <luisenramos0805@gmail.com>
2026-06-01 10:35:38 -0500

    quicksort changed

--- src/quicksort.c
+++ src/quicksort.c
@@ -1,4 +1,6 @@
 #include "sort.h"
+#include <assert.h>
+#include <stdbool.h>
 
 static void swapInt(int *a, int *b){
   int tmp = *a;
@@ -6,6 +8,11 @@ static void swapInt(int *a, int *b){
   *b = tmp;
 }
 
+static int checkPartition(int* arr, int left, int right, int j, int pivot){
+  for(int i = left; i < j; i++) if(arr[i] > pivot) return false;
+  for(int i = j + 1; i < right; i++) if(arr[i] <= pivot) return false;
+  return true;
+}
 static int partition(int arr[], int left, int right){
   /*
    * TODO: complete la partición de quicksort.
@@ -14,8 +21,18 @@ static int partition(int arr[], int left, int right){
    * 2. Mantenga a la izquierda los elementos <= pivote.
    * 3. Devuelva la posición final del pivote.
    */
-  (void)arr;
-  (void)left;
+  int pivot = arr[right];
+  int i = right;
+  int j = right;
+
+  while(left < i){
+    assert(checkPartition(arr, i + 1, right - 1, j, pivot));
+    if(arr[i - 1] > pivot)
+      swapInt(&arr[i], &arr[right]);
+    assert(checkPartition(arr, i, right - 1, j, pivot));
+    i--;
+  }
+  swapInt(&arr[j], &arr[right]);
   return right;
 }
 
